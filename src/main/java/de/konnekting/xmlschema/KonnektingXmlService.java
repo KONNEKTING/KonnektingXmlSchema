f6cf1bc3b6c743751ce0ae7ce41a34716509089b
tuxedo0801 <alex@root1.de>
2015-12-17 15:04:36 +0100

    added missing gpl header

--- src/main/java/de/konnekting/xmlschema/KonnektingXmlService.java
+++ src/main/java/de/konnekting/xmlschema/KonnektingXmlService.java
@@ -1,10 +1,23 @@
+/*
+ * Copyright (C) 2015 Alexander Christian <alex(at)root1.de>. All rights reserved.
+ * 
+ * This file is part of KONNEKTING XML Schema.
+ *
+ *   KONNEKTING XML Schema is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   KONNEKTING XML Schema is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with KONNEKTING XML Schema.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.konnekting.xmlschema;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import de.konnekting.xml.schema.konnekting.KONNEKTING;
 import java.io.File;
 import javax.xml.XMLConstants;
