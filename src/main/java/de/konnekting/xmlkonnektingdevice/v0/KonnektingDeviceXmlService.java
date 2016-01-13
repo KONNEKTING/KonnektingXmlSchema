9b46a20b28c82f3e881c9400105025f6ec7fef8e
achristian <achristian@buero>
2016-01-13 21:09:25 +0100

    updated service class: moved to better package name

--- src/main/java/de/konnekting/xmlkonnektingdevice/v0/KonnektingDeviceXmlService.java
+++ src/main/java/de/konnekting/xmlkonnektingdevice/v0/KonnektingDeviceXmlService.java
@@ -16,30 +16,36 @@
  *   You should have received a copy of the GNU General Public License
  *   along with KonnektingDevice XML Schema.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.konnekting.xml;
+package de.konnekting.xmlkonnektingdevice.v0;
 
 import de.konnekting.xml.konnektingdevice.v0.KonnektingDevice;
 import java.io.File;
+import java.io.StringReader;
+import java.net.URL;
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.util.ValidationEventCollector;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  *
  * @author achristian
  */
-public class KonnektingXmlService {
+public class KonnektingDeviceXmlService {
+    
+    private static final URL XSD_KONNEKTINGDEVICE_V0 = KonnektingDeviceXmlService.class.getResource("/META-INF/xsd/KonnektingDeviceV0.xsd");
 
     private static <T> T unmarshal(String xmlDatei, Class<T> clss)
-        throws JAXBException, SAXException {
+            throws JAXBException, SAXException {
         // Schema und JAXBContext sind multithreadingsicher ("thread safe"):
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(KonnektingXmlService.class.getResource("/META-INF/xsd/KonnektingDeviceV0.xsd"));
+        Schema schema = schemaFactory.newSchema(XSD_KONNEKTINGDEVICE_V0);
         JAXBContext jaxbContext = JAXBContext.newInstance(clss.getPackage().getName());
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
         unmarshaller.setSchema(schema);
@@ -48,9 +54,9 @@ public class KonnektingXmlService {
     }
 
     private static void marshal(String xmlDatei, Object jaxbElement)
-        throws JAXBException, SAXException {
+            throws JAXBException, SAXException {
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(KonnektingXmlService.class.getResource("/META-INF/xsd/KonnektingDeviceV0.xsd"));
+        Schema schema = schemaFactory.newSchema(XSD_KONNEKTINGDEVICE_V0);
         JAXBContext jaxbContext = JAXBContext.newInstance(jaxbElement.getClass().getPackage().getName());
         Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.setSchema(schema);
@@ -62,9 +68,24 @@ public class KonnektingXmlService {
     public static KonnektingDevice readConfiguration(File f) throws JAXBException, SAXException {
         return unmarshal(f.getAbsolutePath(), KonnektingDevice.class);
     }
-    
+
     public static void writeConfiguration(File f, KonnektingDevice konnekt) throws JAXBException, SAXException {
         marshal(f.getAbsolutePath(), konnekt);
+    }
+
+    public static void validateWrite(KonnektingDevice jaxbElement) throws SAXException, JAXBException {
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(XSD_KONNEKTINGDEVICE_V0);
+        
+        JAXBContext jaxbContext = JAXBContext.newInstance(jaxbElement.getClass().getPackage().getName());
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setSchema(schema);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(jaxbElement, new DefaultHandler());
+        System.out.println("Validate: OK");
+
     }
 
 }
